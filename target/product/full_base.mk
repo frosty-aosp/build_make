36c9e099ab860937b1b08c0af809b2ab6ab6d343
Amy King <amy@amyrom.tech>
2026-06-09 04:58:12 +0100

    build_make: refactor to frosty
    Change-Id: Ifd90f03a70fdeea7ce82455142c186ff3c5bf54a

--- target/product/full_base.mk
+++ target/product/full_base.mk
@@ -45,7 +45,7 @@ PRODUCT_VENDOR_PROPERTIES := \
 PRODUCT_LOCALES := en_US
 
 # Get some sounds
-ifeq ($(AICP_BUILD),)
+ifeq ($(FROSTY_BUILD),)
 $(call inherit-product-if-exists, frameworks/base/data/sounds/AllAudio.mk)
 endif
 
--- target/product/gsi_release.mk
+++ target/product/gsi_release.mk
@@ -27,8 +27,8 @@
 #
 
 BUILDING_GSI := true
-ifneq ($(filter aicp_gsi_%,$(TARGET_PRODUCT)),)
-BUILDING_AICP_GSI := true
+ifneq ($(filter frosty_gsi_%,$(TARGET_PRODUCT)),)
+BUILDING_FROSTY_GSI := true
 endif
 
 PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
